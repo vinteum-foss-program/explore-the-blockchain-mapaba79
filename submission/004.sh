f759aae2374c7e54314a768b2d4d997be7c8b2a7
mapaba79 <mateuspaimdebarros23121997@gmail.com>
2024-11-23 17:58:46 -0300

    resolution of script 004

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,4 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#q4q7qurj" "[100,100]" | jq -r '.[0]'
